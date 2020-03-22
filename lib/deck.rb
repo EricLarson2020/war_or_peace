74fd703430ade93b6ada7dce3cc5b2466866eba0
Eric Larson <ericlarsonbroom@gmail.com>
2020-03-22 15:46:37 -0600

    updated indentation

--- lib/deck.rb
+++ lib/deck.rb
@@ -6,13 +6,13 @@ class Deck
     @cards = cards
   end
 
-#index returns the first object in an array such that
+
   def rank_of_card_at(index)
     if cards.length < 3 && index == 2
       return nil
     else
-    cards[index].rank
-  end
+      cards[index].rank
+    end
   end
 
 
@@ -24,28 +24,17 @@ class Deck
 
 
   def percent_high_ranking
-
-  high_rank = cards.select do |card|
+    high_rank = cards.select do |card|
      card.rank > 11
+   end
+   (high_rank.length.to_f/cards.length.to_f).round(3) * 100
   end
-    (high_rank.length.to_f/cards.length.to_f).round(3) * 100
-  end
-
 
   def remove_card
-cards.shift
-   end
+    cards.shift
+  end
 
  def add_card(card)
    cards.append(card)
   end
 end
-
-
-
-
-# def high_ranking_cards
-# cards.select do |card|
-#      card.rank > 11
-#    end
-# end
